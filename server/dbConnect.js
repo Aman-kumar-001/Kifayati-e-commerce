c041d6286e44c57e730e115ebfbbb495244b6ab5
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-07-28 21:25:22 +0530

    done

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose")
 
 async function getConnect(){
     try {
-        // mongoose.connect("mongodb://127.0.0.1:27017/server_4_pm_wd_sept_23")
-        await mongoose.connect("mongodb://127.0.0.1:27017/server")
+        await mongoose.connect("mongodb://127.0.0.1:27017/kifayati")
         console.log("DataBase is Connected!!!")
     } catch (error) {
         console.log(error)
